5347b689be5190d7baaa5f3b1e0b2d58989aa76c
jd99iam <96513365+jd99iam@users.noreply.github.com>
2022-03-25 13:30:18 +0900

    datastructureWeek4HW

--- 1번.c
+++ 1번.c
@@ -1,26 +1,27 @@
-#include <stdio.h>
-
-int main()
-{
-	func(9, 6666);
-
-	return 0;
-}
-
-int func(int a, int b) 
-{
-	if (a <= 1 || a >= 10000 || b <= 1 || b >= 10000)
-	{
-		printf("a,b는 1과 10000사이의 값이어야 합니다.\n");
-		return 0;
-	}
-	
-	int temp = b;
-	
-	for (; temp != 0; temp /= 10) //10으로 나눈 몫을 temp에 저장하며 뒤에 1자리씩 자름 
-	{
-		printf("%d\n", a * (temp % 10)); //일의자리를 a에 곱해서 출력 
-	}
-	printf("%d\n", a * b);
-
-}
+//#include <stdio.h>
+//void func(int* arr, int size)
+//{
+//	int sum = 0;
+//	for (int i = 0; i < size; i++)
+//	{
+//		sum += arr[i];
+//	}
+//	int ave = sum / size;
+//	printf("평균 : %d\n", ave);
+//	int count = 0;
+//	for (int i = 0; i < size; i++)
+//	{
+//		if (arr[i] < ave)
+//		{
+//			count += 1;
+//		}
+//	}
+//	printf("평균보다 작은 원소 : %d개\n", count);
+//}
+//
+//int main()
+//{
+//	int arr[5] = { 1,2,3,4,5 };
+//	func(arr, sizeof(arr)/sizeof(int));
+//	return 0;
+//}
